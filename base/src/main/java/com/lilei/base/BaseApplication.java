ffa21d3c23de44396b082278f5e76b82731621b6
lilei <1136447460@qq.com>
2018-10-26 23:22:09 +0800

    Activity

--- base/src/main/java/com/lilei/base/BaseApplication.java
+++ base/src/main/java/com/lilei/base/BaseApplication.java
@@ -1,6 +1,10 @@
 package com.lilei.base;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Bundle;
+
+import java.lang.ref.WeakReference;
 
 /**
  * Created by lilei
@@ -8,8 +12,55 @@ import android.app.Application;
  */
 
 public class BaseApplication extends Application {
+
+    private static WeakReference<Activity> mTopActivity = null;
+    private Activity topActivityCopyed = null;
     @Override
     public void onCreate() {
         super.onCreate();
+
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
+                mTopActivity = new WeakReference<Activity>(activity);
+                topActivityCopyed = activity;
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                mTopActivity = null;  // help Gc
+                topActivityCopyed = null;
+            }
+        });
     }
+
+    public static WeakReference<Activity> getmTopActivity() {
+        return mTopActivity;
+    }
+
 }
